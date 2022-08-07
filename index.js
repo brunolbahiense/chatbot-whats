d86d253e906df7bc144004c383a732fe457b2cfa
Bruno Luís Bahiense <brunoluisbahiense@gmail.com>
2022-08-07 01:55:01 -0300

    handlers added

--- index.js
+++ index.js
@@ -6,35 +6,45 @@ const client = new Client()
 client.on('qr', qr => qrcode.generate(qr, {small: true}))
 client.on('ready', () => console.log('Olá senhor, estou atento à tudo! 🕵🏼'))
 
+
+
 client.on('message', message => {
     //Configs
     const {author, body, deviceType} = message
+    const name = message._data.notifyName
     const Msg = body.toLowerCase()
     const hello = msgs.includes(Msg)
 
+    const handleSend = txt => {
+        client.sendMessage(message.from, txt) 
+    }
+    
+    const handleReply = txt => {
+        message.reply(txt) 
+    }
+
     
     // Geral
-    if(Msg === 'obrigado' || Msg === 'obrigada') return message.reply('de nada 😄')
-    if(Msg === 'valeu' || Msg === 'vlw') return message.reply('tamo junto 😎')
+    if(Msg === 'obrigado' || Msg === 'obrigada') return handleReply('de nada 😄')
+    if(Msg === 'valeu' || Msg === 'vlw') return handleReply('tmj 😎')
 
 
     const salt = '' // hash localizado em message.id.remote do grupo que quer
     let array = message.id.remote.split('-')
-    console.log(message.id.remote, array)
-    if(array[1] === salt && Msg === '!teste') return message.reply('teste')
+    if(array[1] === salt && Msg === '!teste') return handleReply('teste')
     
     // APENAS EM PRIVADO
     if(author !== undefined) return 
 
     if(Msg === '!info') {
-        return message.reply(
-            `Olá ${message._data.notifyName}! abre o 👀, sei que esta usando um  ${deviceType === 'ios' ? 'iphone' : deviceType}`
+        return handleReply(
+            `Olá ${name}! abre o 👀, sei que esta usando um  ${deviceType === 'ios' ? 'iphone' : deviceType}`
         )
     }
 
-    if(hello) return message.reply('Oi! tudo bem?')
+    if(hello) return handleSend('Oi! tudo bem?')  
 
-    if(Msg === '!help') return message.reply('comandos:\n!help\n!info')
+    if(Msg === '!help') return handleSend('comandos:\n!help\n!info')
 })
 
 client.initialize()
